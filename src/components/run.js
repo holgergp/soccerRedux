21525cb25761d6a7e2266461ceac1972c4f47899
Holger Grosse-Plankermann <holgergp@gmail.com>
2017-03-25 19:03:29 +0100

    Fixing Typo

--- src/components/run.js
+++ src/components/run.js
@@ -1,8 +1,8 @@
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 /*eslint-disable no-unused-vars*/
 import React from 'react'
   /*eslint-enable no-unused-vars*/;
 import App from './App';
 
 // Render the main component into the dom
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
